a9794f4cac9ab67ecc2dbdd006baf20615c1bcfb
Brian Harding <brianmharding13@gmail.com>
2023-04-14 15:42:27 -0500

    variant

--- components/modules/VariantPicker.tsx
+++ components/modules/VariantPicker.tsx
@@ -1,3 +1,4 @@
+//@ts-ignore
 const VariantPicker = ({ variants, ...props }) => {
   if (variants.length === (0 || 1)) return null;
 
@@ -6,6 +7,7 @@ const VariantPicker = ({ variants, ...props }) => {
       {...props}
       className="form-select appearance-none w-full relative mb-3 sm:mb-0 flex-grow sm:mr-3 pl-3 py-2 bg-white border border-gray-300 focus:border-gray-500 shadow-sm text-gray-500 text-sm focus:outline-none focus:text-gray-900 rounded ring-0 focus:ring-0"
     >
+      {/*@ts-ignore*/}
       {variants.map(({ external_id, name }) => (
         <option key={external_id} value={external_id}>
           {name}
