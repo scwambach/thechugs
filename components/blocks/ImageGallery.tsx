0ea8da36f699b8b9122650c9e08d93e3675a8105
Scott Wambach <scott@scottwamba.ch>
2023-04-15 09:30:00 -0500

    image gallery

--- components/blocks/ImageGallery.tsx
+++ components/blocks/ImageGallery.tsx
@@ -5,24 +5,35 @@ import { ImageProps } from '@utils/types'
 import { urlFor } from '@utils/urlFor'
 import Zoom from 'react-medium-image-zoom'
 import 'react-medium-image-zoom/dist/styles.css'
+import { useWindowWidth } from '@utils/useWindowWidth'
 
 const ImageGallery = ({ images }: { images: ImageProps[] }) => {
+  const windowWidth = useWindowWidth()
   return (
     <section id="images" className="imageGallery">
-      <Container maxWidth={breakpoints.lrgx}>
+      <Container maxWidth={breakpoints.xxl}>
         {images.map((img, index: number) => (
           <Zoom
             key={img._key}
+            zoomMargin={windowWidth > breakpoints.lg ? 120 : 20}
             zoomImg={{
               alt: '',
               src: `${urlFor(img).width(1200).height(700).fit('crop')}`,
             }}
-            zoomMargin={120}
           >
             <ResponsiveImage
               width={1200}
-              height={800}
-              src={`${urlFor(img).width(1200).quality(80)}`}
+              height={900}
+              lqip={`${urlFor(img)
+                .width(12)
+                .height(9)
+                .quality(80)
+                .fit('crop')}`}
+              src={`${urlFor(img)
+                .width(1200)
+                .height(900)
+                .quality(80)
+                .fit('crop')}`}
               alt={`Gallery Image ${index + 1}`}
             />
           </Zoom>
