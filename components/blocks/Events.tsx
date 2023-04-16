1cd0a1b4892d15c87555101e49cd13dbe50db6dc
Scott Wambach <scott@scottwamba.ch>
2023-04-16 12:02:01 -0500

    updated Events.tsx

--- components/blocks/Events.tsx
+++ components/blocks/Events.tsx
@@ -1,7 +1,7 @@
 import { Container } from '@components/modules/Container'
 import { breakpoints } from '@utils/settings'
 import { EventProps } from '@utils/types'
-import { Event } from '@components/modules/Event'
+// import { Event } from '@components/modules/Event'
 
 const Events = ({ events }: { events: EventProps[] }) => {
   return (
@@ -10,7 +10,8 @@ const Events = ({ events }: { events: EventProps[] }) => {
         <ul className="unstyled">
           {events.map((event) => (
             <li key={event._id}>
-              <Event {...event} />
+              test
+              {/* <Event {...event} /> */}
             </li>
           ))}
         </ul>
