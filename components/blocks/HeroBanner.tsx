84d56c7452e3973317bcb1b9bcef132e0a10d348
Scott Wambach <scott@scottwamba.ch>
2023-04-20 07:48:05 -0500

    logo animation

--- components/blocks/HeroBanner.tsx
+++ components/blocks/HeroBanner.tsx
@@ -2,8 +2,9 @@ import { BlurImage } from '@components/modules/BlurImage'
 import { ResponsiveImage } from '@components/modules/ResponsiveImage'
 import { BannerProps } from '@utils/types'
 import { urlFor } from '@utils/urlFor'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import ReactPlayer from 'react-player'
+import VanillaTilt from 'vanilla-tilt'
 
 const HeroBanner = ({ heading, image, logo, video }: BannerProps) => {
   const [hasWindow, setHasWindow] = useState<boolean>(false)
@@ -26,6 +27,20 @@ const HeroBanner = ({ heading, image, logo, video }: BannerProps) => {
       }
     }
   }, [visible, hasWindow])
+
+  const tiltOptions = {
+    scale: 1.08,
+    speed: 1000,
+    glare: false,
+    max: 5,
+    reverse: true,
+  }
+  const tilt = useRef(null)
+
+  useEffect(() => {
+    if (!tilt.current) return
+    VanillaTilt.init(tilt.current, tiltOptions)
+  }, [])
   return (
     <section className="hero-banner">
       <div className="inner">
@@ -43,7 +58,7 @@ const HeroBanner = ({ heading, image, logo, video }: BannerProps) => {
           <BlurImage isBackground {...image} />
         </div>
 
-        <div className="logo">
+        <div className="logo" ref={tilt}>
           <h1 className="sr-only">{heading}</h1>
           <ResponsiveImage
             src={`${urlFor(logo)}`}
