d55eee5ae9868c7216750608919f595fc527d0ba
Brian Harding <brianmharding13@gmail.com>
2023-06-13 14:41:38 -0500

    Price check on sanity products

--- pages/api/products/[id].ts
+++ pages/api/products/[id].ts
@@ -1,6 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-
+import { client } from '@utils/client'
 import { printful } from "@lib/printful-client";
+import moment from "moment";
+import { PRODUCTS_QUERY } from "queries/products";
 
 type Data = {
   id: string;
@@ -20,16 +22,30 @@ export default async function handler(
   const { id } = req.query;
 
   try {
-    const { result } = await printful.get(`store/variants/@${id}`);
+    const today = moment(new Date()).format('YYYY-MM-DD')
+    const sanityProducts = await client.fetch(PRODUCTS_QUERY, { today });
+    const sanityProduct = sanityProducts?.find((x: any) => x._id === id);
 
     res.setHeader("Cache-Control", "s-maxage=3600, stale-while-revalidate");
 
-    res.status(200).json({
-      id: id as string,
-      price: result.retail_price,
-      url: `/api/products/${id}`,
-      customFields: []
-    });
+    if (sanityProduct) {
+      res.status(200).json({
+        id: id as string,
+        price: sanityProduct.price,
+        url: `/api/products/${id}`,
+        customFields: []
+      });
+    } else {
+      const { result } = await printful.get(`store/variants/@${id}`);
+      res.status(200).json({
+        id: id as string,
+        price: result.retail_price,
+        url: `/api/products/${id}`,
+        customFields: [],
+      });
+    }
+
+
   } catch (error: any) {
     console.log(error);
     res.status(404).json({
