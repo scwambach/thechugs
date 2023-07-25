b43cfdb183ce0d164f5a99f46571f2a7efcf7cf5
Brian Harding <brianmharding13@gmail.com>
2023-07-25 15:45:27 -0500

    swap GA implementations

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,12 @@
 import '@styles/main.scss'
 import { WishlistProvider } from 'context/wishlist'
 import type { AppProps } from 'next/app'
-import Script from 'next/script'
+import { GoogleAnalytics } from 'nextjs-google-analytics'
 
 export default function App({ Component, pageProps }: AppProps) {
-  const ga = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS
   return (
     <>
-
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${ga}`}
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', '${ga}');
-        `}
-      </Script>
+      <GoogleAnalytics trackPageViews />
       <WishlistProvider>
         <Component {...pageProps} />
       </WishlistProvider>
