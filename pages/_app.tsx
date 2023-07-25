501db26d81cfb0c43c1b621e5e2a940426cca109
Brian Harding <brianmharding13@users.noreply.github.com>
2023-07-25 10:14:28 -0500

    add GA (#18)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,28 @@
 import '@styles/main.scss'
 import { WishlistProvider } from 'context/wishlist'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <WishlistProvider>
-      <Component {...pageProps} />
-    </WishlistProvider>
+    <>
+
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-SFN3FMNWT8"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-SFN3FMNWT8');
+        `}
+      </Script>
+      <WishlistProvider>
+        <Component {...pageProps} />
+      </WishlistProvider>
+    </>
   )
 }
