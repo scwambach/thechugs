88c5a852654758329b66451ca5f4ea7f7e51be07
Brian Harding <brianmharding13@gmail.com>
2023-08-03 16:05:04 -0500

    clean up

--- pages/api/scraper.ts
+++ pages/api/scraper.ts
@@ -1,13 +1,12 @@
-// pages/api/scrape-playlists.ts
 import { SpotifyPlaylist } from '@utils/types'
 import { NextApiRequest, NextApiResponse } from 'next'
 import puppeteer from 'puppeteer'
 
-const IS_PRODUCTION = process.env.NEXT_PUBLIC_ENV === 'prod';
+const IS_PRODUCTION = process.env.NEXT_PUBLIC_ENV === 'prod'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'GET') {
-    return res.status(405).end() // Method Not Allowed
+    return res.status(405).end()
   }
 
   const { id } = req.query
