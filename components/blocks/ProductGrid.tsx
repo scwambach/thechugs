35a93e143db6fddebc24fb797d37ddb7ad8c3c3d
Brian Harding <brianmharding13@gmail.com>
2023-08-19 11:16:38 -0500

    QR to product in site

--- components/blocks/ProductGrid.tsx
+++ components/blocks/ProductGrid.tsx
@@ -1,32 +1,50 @@
 /* eslint-disable */
+// @ts-nocheck
 import { Container } from '@components/modules/Container'
 import PrintfulProduct from './PrintfulProduct'
 import { breakpoints } from '@utils/settings'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import SanityProduct from './SanityProduct'
+import { useRouter } from 'next/router'
 
 const ProductGrid = ({ products }: { products: any }) => {
   if (!products || products.length === 0) return null
+  const router = useRouter()
+  const [queryId, setQueryId] = useState('')
+  const [orderedProducts, setOrderedProducts] = useState([])
+  const [queryProd, setQueryProd] = useState(null)
 
-  const orderedProducts = products.sort((a: any, b: any) => {
-    a = {
-      ...a,
-      name: a.name?.replace('The Chugs', '').toUpperCase(),
-      title: a.title?.replace('The Chugs', '').toUpperCase()
+  useEffect(() => {
+    const orderProds = products.sort((a: any, b: any) => {
+      a = {
+        ...a,
+        name: a.name?.replace('The Chugs', '').toUpperCase(),
+        title: a.title?.replace('The Chugs', '').toUpperCase()
+      }
+      b = {
+        ...b,
+        name: b.name?.replace('The Chugs', '').toUpperCase(),
+        title: a.title?.replace('The Chugs', '').toUpperCase()
+      }
+      return (a.name < b.name) ? -1 : (a.name > b.name) ? 1 : 0;
+    })
+    setOrderedProducts(orderProds);
+
+    const { prodId } = router.query
+    if (prodId !== '') {
+      const idAsNum = parseInt(prodId);
+      console.log(orderProds)
+      console.log(orderProds.find((x:any) => x.id === idAsNum || x._id === prodId))
+      setQueryProd(orderProds.find((x:any) => x.id === idAsNum || x._id === prodId))
     }
-    b = {
-      ...b,
-      name: b.name?.replace('The Chugs', '').toUpperCase(),
-      title: a.title?.replace('The Chugs', '').toUpperCase()
-    }
-    return (a.name < b.name) ? -1 : (a.name > b.name) ? 1 : 0;
-  });
+
+  }, [])
 
   return (
     <section id="products" className="products">
       <Container maxWidth={breakpoints.xxl}>
         <div className="product-grid">
-          {orderedProducts.map((product: any) => (
+          {orderedProducts?.map((product: any) => (
             <React.Fragment
               key={product.external_id ? product.id : product._id}
             >
@@ -39,6 +57,20 @@ const ProductGrid = ({ products }: { products: any }) => {
           ))}
         </div>
       </Container>
+      {queryProd && (
+        <>
+          <div className="modal-bg" onClick={() => setQueryProd(null)}></div>
+          <div className="modal">
+            <div className="modal-container">
+              {queryProd.external_id ? (
+                <PrintfulProduct key={queryProd.id} {...queryProd} />
+              ) : (
+                <SanityProduct key={queryProd._id} {...queryProd} />
+              )}
+            </div>
+          </div>
+        </>
+      )}
     </section>
   )
 }
