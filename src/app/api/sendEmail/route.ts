2e8c6915ecac35a7696424795c36dafe17497263
Scott Wambach <scott@scottwamba.ch>
2023-11-11 11:22:14 -0600

    mailer

--- src/app/api/sendEmail/route.ts
+++ src/app/api/sendEmail/route.ts
@@ -1,10 +1,80 @@
-import { NextResponse } from 'next/server'
+import nodemailer from 'nodemailer'
+import { NextRequest, NextResponse } from 'next/server'
 
-export async function POST(req: Request) {
-  const body = await req.json()
+export async function POST(request: NextRequest) {
+  const formData = await request.json()
 
-  return NextResponse.json({
-    status: 200,
-    body: 'Email sent!',
-  })
+  const formDataArray = Object.entries(formData.fields)
+
+  const htmlMessage = `<ul>${formDataArray
+    .map((pair) => {
+      return `<li>${pair[0].split('_')[0]}: ${pair[1]}</li>`
+    })
+    .join('')}</ul>`
+
+  const textMessage = `${formDataArray.map((pair) => {
+    return `${pair[0]}: ${pair[1]}`
+  })}`
+
+  async function main() {
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      host: 'smtp.gmail.com',
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.GOOGLE_EMAIL,
+        pass: process.env.GOOGLE_APP_PASSWORD,
+      },
+    })
+
+    const info = await transporter.sendMail({
+      from: `"The Chugs ${formData.subject}" <${process.env.GOOGLE_EMAIL}>`,
+      to: formData.recipients,
+      subject: formData.subject,
+      text: textMessage,
+      html: htmlMessage,
+    })
+
+    return {
+      success: true,
+      message: { info },
+    }
+  }
+
+  const sendMail = await main()
+  const { success } = sendMail
+
+  async function postToSlack() {
+    await fetch(
+      `https://hooks.slack.com/services/${process.env.SLACK_WEBHOOK}`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          text: `The form ${formData.subject} has messed up somehow. Go check https://vercel.com/wambach-web-development/thechugs to see what HECK is going on!!.`,
+        }),
+      }
+    )
+  }
+
+  if (success) {
+    return NextResponse.json({
+      status: 200,
+      body: {
+        message: 'Your message has been sent!',
+      },
+    })
+  } else {
+    postToSlack()
+
+    return NextResponse.json({
+      status: 500,
+      body: {
+        message: 'Your message could not be sent.',
+      },
+    })
+  }
 }
--- src/components/blocks/Form.tsx
+++ src/components/blocks/Form.tsx
@@ -1,8 +1,10 @@
+'use client'
 import { Container } from '@components/modules/Container'
 import { FormField } from '@components/modules/FormField'
 import { PortableText } from '@portabletext/react'
 import { PortableTextBlock } from '@portabletext/types'
 import { FormFieldProps } from '@utils/types/modules/FormFieldProps'
+import { useState } from 'react'
 
 interface FormProps {
   _key: string
@@ -16,8 +18,6 @@ interface FormProps {
   title: string
 }
 
-// TODO: Add form functionality
-
 export const Form = ({
   _key,
   beforeSubmit,
@@ -29,6 +29,29 @@ export const Form = ({
   thankYouMessage,
   title,
 }: FormProps) => {
+  const [formFieldsValues, setFormFieldsValues] = useState<any>({})
+  const [formResponse, setFormResponse] = useState<any>(null)
+
+  const handleSubmit = async () => {
+    const response = await fetch('/api/sendEmail', {
+      method: 'POST',
+      body: JSON.stringify({
+        fields: {
+          ...formFieldsValues,
+        },
+        recipients,
+        subject: title,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    const data = await response.json()
+    setFormResponse(data)
+
+    return data
+  }
+
   return (
     <div className="form">
       <Container size="wide">
@@ -38,19 +61,52 @@ export const Form = ({
             <PortableText value={description} />
           </div>
         )}
-        <form id={`form_${_key}`}>
-          <div className="fields">
-            {formFields.map((field, index) => (
-              <FormField key={field._key} {...field} fieldIndex={index} />
-            ))}
-            {beforeSubmit && (
-              <div className="beforeSubmit">
-                <PortableText value={beforeSubmit} />
+        {formResponse ? (
+          <div className="response">
+            {formResponse.status === 200 ? (
+              <div className="success">
+                <p>{thankYouMessage}</p>
+              </div>
+            ) : (
+              <div className="error">
+                <p>{errorMessage}</p>
               </div>
             )}
           </div>
-          <button type="submit">{submitCopy}</button>
-        </form>
+        ) : (
+          <form
+            id={`form_${_key}`}
+            onSubmit={(e) => {
+              e.preventDefault()
+              handleSubmit()
+            }}
+            onChange={() => {
+              const form = document.getElementById(
+                `form_${_key}`
+              ) as HTMLFormElement
+              if (form) {
+                const formValues = new FormData(form)
+                const values: any = {}
+                formValues.forEach((value, key) => {
+                  values[key] = value
+                })
+                setFormFieldsValues(values)
+              }
+            }}
+          >
+            <div className="fields">
+              {formFields.map((field, index) => (
+                <FormField key={field._key} {...field} fieldIndex={index} />
+              ))}
+              {beforeSubmit && (
+                <div className="beforeSubmit">
+                  <PortableText value={beforeSubmit} />
+                </div>
+              )}
+            </div>
+            <button type="submit">{submitCopy}</button>
+          </form>
+        )}
       </Container>
     </div>
   )
