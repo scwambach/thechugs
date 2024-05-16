c75c4c094f1746664654690fa0051fbe1f7772ac
Scott Wambach <scott@scottwamba.ch>
2024-05-16 13:21:02 -0500

    meta data

--- src/app/(site)/[slug]/page.tsx
+++ src/app/(site)/[slug]/page.tsx
@@ -2,6 +2,7 @@ import { PageFactory } from '@components/global/PageFacorty'
 import { PageTemplate } from '@components/global/PageTemplate'
 import { client } from '@utils/client'
 import { PAGE_QUERY } from '@utils/queries/PAGE_QUERY'
+import { urlFor } from '@utils/urlFor'
 import { notFound } from 'next/navigation'
 
 export default async function Page({ params }: { params: { slug: string } }) {
@@ -31,6 +32,28 @@ async function getData(slug: string) {
 }
 
 export const revalidate = 0
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string }
+}) {
+  const { slug } = params
+  const data = await client.fetch(PAGE_QUERY, {
+    slug,
+  })
+
+  return {
+    title: `${data.title} | The Chugs - The Band... Refreshing!`,
+    description: data.description,
+    openGraph: {
+      images: `${urlFor(data.pageImage).width(600)}`,
+    },
+    icons: {
+      icon: '/favicon.png',
+    },
+  }
+}
 
 export async function generateStaticParams() {
   const pageSlugs = await client.fetch(`*[_type == 'page'].slug.current`)
