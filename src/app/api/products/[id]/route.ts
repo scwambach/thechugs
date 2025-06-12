fb781fe57d5d83eddd98c38887cbbe9ff35fc1c9
Scott Wambach <scott@scottwamba.ch>
2025-06-12 12:33:45 -0500

    day

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,14 +1,14 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { client } from '@utils/client'
 import { printful } from '@lib/printful-client'
-import moment from 'moment'
+import dayjs from 'dayjs'
 import { NextRequest, NextResponse } from 'next/server'
 import { productsQuery } from '@utils/queries/products'
 
 export async function GET(req: NextRequest) {
   const id = req.url.split('/')[req.url.split('/').length - 1]
   try {
-    const today = moment(new Date()).format('YYYY-MM-DD')
+    const today = dayjs(new Date()).format('YYYY-MM-DD')
     const sanityProducts = await client.fetch(productsQuery, { today })
     const sanityProduct = sanityProducts?.find((x: any) => x._id === id)
     if (sanityProduct) {
